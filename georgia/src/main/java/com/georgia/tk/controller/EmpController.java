5fef2fbe1d4fda7e0647bbda4926fe86d53480e0
weather1212 <60419960+weather1212@users.noreply.github.com>
2020-09-17 13:38:57 +0900

    Update EmpController.java

--- georgia/src/main/java/com/georgia/tk/controller/EmpController.java
+++ georgia/src/main/java/com/georgia/tk/controller/EmpController.java
@@ -1,5 +1,7 @@
 package com.georgia.tk.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.georgia.tk.emp.EmpService;
+import com.georgia.tk.emp.EmpVO;
 import com.georgia.tk.profit.ProfitService;
 import com.georgia.tk.project.ProjectService;
 @Controller
@@ -15,9 +18,19 @@ public class EmpController {
 	private EmpService empService;
 	
 	
-	@RequestMapping("/test")
-	public String test(HttpServletRequest request) {
-		return "test.jsp";
+	@RequestMapping("/empList")
+	public String test(String page, EmpVO empVO, HttpServletRequest request) {
+		if(page==null || page.equals("1")) {
+			page="1";
+			empVO.setPage(0);
+		} else {
+			empVO.setPage((Integer.parseInt(page)-1)*20);
+		}
+		List<EmpVO> empList = empService.listSelectEmp(empVO);
+		request.setAttribute("empList", empList);
+		request.setAttribute("pageCnt", 2);
+		request.setAttribute("page", page);
+		return "WEB-INF/views/emp/empList.jsp";
 	}
 	
 }
