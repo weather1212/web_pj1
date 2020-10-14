e98a4433798d18cb2b63911be7fc34114ccda66c
weather1212 <60419960+weather1212@users.noreply.github.com>
2020-10-14 11:15:42 +0900

    add empSearch and mod pageCnt

--- georgia/src/main/java/com/georgia/tk/controller/EmpController.java
+++ georgia/src/main/java/com/georgia/tk/controller/EmpController.java
@@ -1,6 +1,8 @@
 package com.georgia.tk.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.georgia.tk.emp.EmpService;
 import com.georgia.tk.emp.EmpVO;
-import com.georgia.tk.profit.ProfitService;
-import com.georgia.tk.project.ProjectService;
 
 @Controller
 //@RequestMapping("/emp/*")
@@ -23,18 +23,78 @@ public class EmpController {
 	private EmpService empService;
 
 	@RequestMapping("/empList")
-	public String test(String page, EmpVO empVO, HttpServletRequest request) {
+	public String empList(String page, EmpVO empVO, HttpServletRequest request) {
 		if (page == null || page.equals("1")) {
 			page = "1";
 			empVO.setPage(0);
 		} else {
 			empVO.setPage((Integer.parseInt(page) - 1) * 20);
 		}
+		System.out.println("검색 게시물 페이지 : " + page);
+		
 		List<EmpVO> empList = empService.empList(empVO);
+
+		int empListCount = empService.empListCount();
+		System.out.println("총 게시물 수 : " + empListCount);
+
+		int pageCnt = 0;
+		if (empListCount % 20 == 0) {
+			pageCnt = empListCount / 20;
+		} else {
+			pageCnt = empListCount / 20 + 1;
+		}
+
 		request.setAttribute("empList", empList);
-		request.setAttribute("pageCnt", 2);
+		request.setAttribute("pageCnt", pageCnt);
 		request.setAttribute("page", page);
+
 		return "WEB-INF/views/emp/empList.jsp";
+	}
+
+	@RequestMapping("/empSearch")
+	public String empSearch(@RequestParam(defaultValue = "") String empId,
+			@RequestParam(defaultValue = "") String empName, @RequestParam(defaultValue = "") String empDepartment,
+			@RequestParam(defaultValue = "") String empPosition, @RequestParam(defaultValue = "") String empHiredate,
+			String page, EmpVO empVO, HttpServletRequest request) {
+		
+		if (page == null || page.equals("1")) {
+			page = "1";
+			empVO.setPage(0);
+		} else {
+			empVO.setPage((Integer.parseInt(page) - 1) * 20);
+		}
+		System.out.println("검색 게시물 페이지 : " + page);
+		
+		Map<String, Object> empSearchMap = new HashMap<String, Object>();
+		empSearchMap.put("empId", empId);
+		empSearchMap.put("empName", empName);
+		empSearchMap.put("empDepartment", empDepartment);
+		empSearchMap.put("empPosition", empPosition);
+		empSearchMap.put("empHiredate", empHiredate);
+		empSearchMap.put("page", empVO.getPage());
+		
+		int empListCount = empService.empSearchCount(empSearchMap);
+		System.out.println("검색 게시물 수 : " + empListCount);
+
+		int pageCnt = 0;
+		if (empListCount % 20 == 0) {
+			pageCnt = empListCount / 20;
+		} else {
+			pageCnt = empListCount / 20 + 1;
+		}
+
+		List<EmpVO> empList = empService.empList(empSearchMap);
+
+		request.setAttribute("empList", empList);
+		request.setAttribute("pageCnt", pageCnt);
+		request.setAttribute("page", page);
+		request.setAttribute("searchEmpId", empId);
+		request.setAttribute("searchEmpName", empName);
+		request.setAttribute("searchEmpDepartment", empDepartment);
+		request.setAttribute("searchEmpPosition", empPosition);
+		request.setAttribute("searchEmpHiredate", empHiredate);
+
+		return "WEB-INF/views/emp/empSearchList.jsp";
 	}
 
 	@RequestMapping("/empDetail")
@@ -67,7 +127,7 @@ public class EmpController {
 
 	@RequestMapping("/empCreate")
 	public String empCreate(@ModelAttribute EmpVO empVO, HttpServletRequest request) {
-		
+
 		System.out.println("sfdfsdf");
 		System.out.println(empVO.getEmp_id());
 		System.out.println(empVO.getEmp_name());
@@ -76,16 +136,10 @@ public class EmpController {
 		System.out.println(empVO.getEmp_hiredate());
 		System.out.println(empVO.getEmp_profit());
 		System.out.println(empVO.getEmp_salary());
-		
+
 		empService.empCreate(empVO);
 
 		return "empList";
-	}
-
-	@RequestMapping("/empSearch")
-	public String empSearch(String page, EmpVO empVO, HttpServletRequest request) {
-
-		return "WEB-INF/views/emp/empSearchList.jsp";
 	}
 
 }
